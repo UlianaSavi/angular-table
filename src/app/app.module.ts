0fae374a511b3293b6de8141bf148b5f75d089f7
Uliana Savitskaua <uljasha.sav@gmail.com>
2024-02-09 15:28:53 +0600

    create gloabl styles && create start page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartPageComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
